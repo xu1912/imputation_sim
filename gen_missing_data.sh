b02ca4fa5f87dac12b1a2a49030ecfcce59d5d47
Chao Xu <xux1912@gmail.com>
2017-04-12 21:47:07 -0500

    Update gen_missing_data.sh

--- gen_missing_data.sh
+++ gen_missing_data.sh
@@ -5,4 +5,8 @@ plink --file h --recode vcf
 shapeit -T 2 --main 20 -V plink_missing.txt -M genetic_map_chr22_combined_b37.txt -O plink_phased.out
 shapeit -convert --input-haps plink_phased.out --output-vcf plink_phased.vcf
 sed -i "s/^0/22/g" plink_phased.vcf
+head -n6 plink_phased.vcf > plink_phased_tmp.vcf
+sed -i "1,6d" plink_phased.vcf
+awk -v OFS='\t' '$3=$1":"$2' plink_phased.vcf >> plink_phased_tmp.vcf
+mv plink_phased_tmp.vcf plink_phased.vcf
 rm -f shapeit_*
