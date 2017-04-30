4ce913e3d56c488b7f3354c72d0fcf82cae16d2f
Chao Xu <xux1912@gmail.com>
2017-04-30 14:06:02 -0500

    Update gen_missing_data.sh

--- gen_missing_data.sh
+++ gen_missing_data.sh
@@ -1,7 +1,26 @@
 # imputation_sim
+#!/bin/bash
+n_case=0
+n_control=1000
+REGION_FL="~/missing_gp/region/"$SN".txt"   	##Region file location
+hap_ref="/home/xc/ngs/hap_ref/"
+out_dir="~/missing_gp"
+mkdir -p $out_dir"/"$SN
+count=0
+while read line;do
+        a[$count]=${line##*:}
+        count=$(( $count + 1 ))
+done < $REGION_FL
+
+causl_l=${a[2]}" 1 1.1 1.2"
+let LOW_BOUND=${a[0]}
+let UP_BOUND=${a[1]}
+
+hapgen2 -m $hap_ref/chr22_combined_b37.txt -l $hap_ref/chr22_EUR.legend -h $hap_ref/chr22_EUR.hap -o $out_dir/$SN/h -dl $causl_l -n $n_case $n_control -int $LOW_BOUND $UP_BOUND >$out_dir/$SN/debug.txt
+
 gtool -G --g h.controls.gen --s h.controls.sample --ped h.ped --map h.map --threshold 0.9
 plink --file h --recode vcf
-./mask.py plink.vcf
+~/missing_gp/mask.py plink.vcf
 shapeit -T 2 --main 20 -V plink_missing.txt -M genetic_map_chr22_combined_b37.txt -O plink_phased.out
 shapeit -convert --input-haps plink_phased.out --output-vcf plink_phased.vcf
 sed -i "s/^0/22/g" plink_phased.vcf
